8003bbd4b86c679d0500197b53cb61caeea03daf
Jaco020 <kubase477@gmail.com>
2021-05-08 17:02:31 +0200

    skonczono podglad.html

---
+++ JavaScript/wykres.js
@@ -0,0 +1,30 @@
+var wykresObiekt = document.getElementById("wykresFrekfencja").getContext("2d");
+const uczenGodzinyData = [2,0,30];
+console.log(Math.max(uczenGodzinyData));
+Chart.defaults.font.size = 15;
+var wykresFrekfencja = new Chart(wykresObiekt,{
+    type: 'bar',
+    data: {
+        labels:["Nieobecności",["Wniosek", "o usprawiedliwienie"],"Usprawiedliwone"],
+        datasets: [{
+            label:'Godziny',
+            data:uczenGodzinyData,
+            backgroundColor:"#2456e0"// --blue-500
+        }]
+    },
+    options:{
+        scales:{
+            y:{
+                max:Math.max(...uczenGodzinyData)+5,
+                ticks: {
+                    stepSize: 5
+                }
+            }
+        },
+        plugins:{
+            legend:{
+                display:false,
+            }
+        }
+    }
+});
